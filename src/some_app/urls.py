73513a5768cf7b529bf35679bc19665db1586861
Alesia <alicelevkovichlev@gmail.com>
2020-12-02 18:26:14 +0300

    Finally done homework18!

--- src/some_app/urls.py
+++ src/some_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from some_app.views_19_4 import index as ix, process_form
+from some_app.views import index as ix, process_form, hello_user, render_name_form as rn, get_name
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     # path('', index, name='index'),
     path('form', ix, name='ix'),
-    path('process_form', process_form, name='process_form')
+    path('process_form', process_form, name='process_form'),
+    path('hello_user', hello_user),
+    path('render_name_form', rn),
+    path('get_name', get_name)
 ]
--- src/some_app/views.py
+++ src/some_app/views.py
@@ -1,14 +1,35 @@
+from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 
 def index(request):
-    name = request.GET.get('name')
-    lastname = request.GET.get('lastname')
-    age = request.GET.get('age')
+    return render(request, 'form.html', {})
 
-    if not age or age and int(age) < 18:
-        return HttpResponse(f'Permission Denied')
 
-    return HttpResponse(f'Welcome {name} {lastname}!')
+def process_form(request):
+    name = request.POST.get('name')
+    age = request.POST.get('age')
+    comment = request.POST.get('comment')
+    string_list = comment.split("\r\n")
+    new_string = [element + f'(c) {name}' for element in string_list]
+    result_string = "\r\n".join(new_string)
+    return render(request, 'process_form.html', {'result_string': result_string})
+
+
+def hello_user(request):
+    template = loader.get_template('hello_user.html')
+    context = {'name': request.GET.get('name')}
+    return HttpResponse(template.render(context, request))
+
+
+def render_name_form(request):
+    return render(request, 'name_form.html')
+
+
+def get_name(request):
+    name = request.POST.get('name')
+    context = {'name': request.POST.get('name')}
+    return render(request, 'process_name_form.html', context)
 
 
