69b30b63f36b513eef3b8d9a99aae54f3e29dfdb
Alesia <alicelevkovichlev@gmail.com>
2020-12-07 14:45:56 +0300

    Request method DELETE was added

--- src/cw20/urls.py
+++ src/cw20/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cw20.views import lines
+from cw20.views import lines, delete_lines
 
 urlpatterns = [
-    path('lines/', lines, name='lines')
+    path('lines/', lines, name='lines'),
+    path('delete_lines/', delete_lines, name='delete_lines')
 ]
--- src/src/settings.py
+++ src/src/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'some_app',
     'cw20',
     'cw21',
+    'cw22'
 ]
 
 MIDDLEWARE = [
